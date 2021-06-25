4fcf687253b05d8f372f8e7e13311fd6502fce25
Sigit Wahyudi <44634025+sgitwhyd@users.noreply.github.com>
2021-06-25 18:49:20 +0700

    php mvc routing

--- app/core/App.php
+++ app/core/App.php
@@ -5,13 +5,15 @@ class App
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+        echo var_dump($url);
     }
 
     public function parseURL()
     {
         if (isset($_GET['url'])) {
-            $url = $_GET['url'];
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
